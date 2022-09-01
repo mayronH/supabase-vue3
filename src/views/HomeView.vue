df808c2f8ee7f9a9a64256474b84a646f936fe4f
mayronH <38799771+mayronH@users.noreply.github.com>
2022-09-01 10:52:36 -0300

    feature: fetch data from supabase and display them in grid layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,61 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import { supabase } from '../lib/supabase'
+import { Smoothie } from '../types'
+import SmoothieCard from '../components/SmoothieCard.vue'
 
-console.log(supabase)
+const fetchError = ref()
+const smoothies = ref<Array<Smoothie> | null>(null)
+
+const fetchSmoothies = async () => {
+  // Fetch all data from 'smoothies' table
+  const { data, error } = await supabase.from('smoothies').select()
+
+  if (error) {
+    fetchError.value = 'Could not fetch the smoothies'
+    smoothies.value = null
+    console.log(error)
+    return
+  }
+  if (data) {
+    smoothies.value = data
+    fetchError.value = null
+  }
+}
+
+onMounted(() => {
+  fetchSmoothies()
+})
 </script>
 <template>
   <main class="content">
-    <h1>Hello World</h1>
+    <div v-if="fetchError" class="error">
+      <p>{{ fetchError }}</p>
+    </div>
+    <div v-else class="smoothies">
+      <SmoothieCard
+        v-for="smoothie in smoothies"
+        :key="smoothie.id"
+        :smoothie="smoothie"
+      ></SmoothieCard>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.smoothies {
+  display: flex;
+  flex-direction: column;
+  gap: var(--medium-size-fluid);
+
+  margin: var(--medium-size-fluid) 0;
+
+  width: 100%;
+}
+@media only screen and (min-width: 768px) {
+  .smoothies {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+</style>
